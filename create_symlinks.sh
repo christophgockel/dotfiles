14e6e80e61c89c319cf46d7e03245ef9b5146d07
Christoph Gockel <github@christophgockel.de>
2015-03-18 11:57:40 +0000

    Fixes variable substitution in symlink script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,21 +4,21 @@ base_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 if [ ! -f ~/.vimrc ]; then
   echo "Linking .vimrc"
-  ln -s $BASE_DIR/vim/vimrc ~/.vimrc
+  ln -s $base_dir/vim/vimrc ~/.vimrc
 else
   echo "Skipping .vimrc"
 fi
 
 if [ ! -f ~/.zshrc ]; then
   echo "Linking .zshrc"
-  ln -s $BASE_DIR/zsh/zshrc ~/.zshrc
+  ln -s $base_dir/zsh/zshrc ~/.zshrc
 else
   echo "Skipping .zshrc"
 fi
 
 if [ ! -f ~/.tmux.conf ]; then
   echo "Linking .tmux.conf"
-  ln -s $BASE_DIR/tmux/tmux.conf ~/.tmux.conf
+  ln -s $base_dir/tmux/tmux.conf ~/.tmux.conf
 else
   echo "Skipping .tmux.conf"
 fi
