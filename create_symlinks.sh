e5d9638119e95355bcb379571b2a3c85f164037a
Christoph Gockel <github@christophgockel.de>
2015-03-21 23:53:09 +0000

    Changes "~" to "$HOME" in setup files.
    This fixes the issue when this repo is cloned and run with Chef.
    Chef passes the proper $HOME value to its recipes but is executed by
    user root.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,23 +2,23 @@
 
 base_dir=$(dirname "$PWD/$0")
 
-if [ ! -f ~/.vimrc ]; then
+if [ ! -f $HOME/.vimrc ]; then
   echo "Linking .vimrc"
-  ln -s $base_dir/vim/vimrc ~/.vimrc
+  ln -s $base_dir/vim/vimrc $HOME/.vimrc
 else
   echo "Skipping .vimrc"
 fi
 
-if [ ! -f ~/.zshrc ]; then
+if [ ! -f $HOME/.zshrc ]; then
   echo "Linking .zshrc"
-  ln -s $base_dir/zsh/zshrc ~/.zshrc
+  ln -s $base_dir/zsh/zshrc $HOME/.zshrc
 else
   echo "Skipping .zshrc"
 fi
 
-if [ ! -f ~/.tmux.conf ]; then
+if [ ! -f $HOME/.tmux.conf ]; then
   echo "Linking .tmux.conf"
-  ln -s $base_dir/tmux/tmux.conf ~/.tmux.conf
+  ln -s $base_dir/tmux/tmux.conf $HOME/.tmux.conf
 else
   echo "Skipping .tmux.conf"
 fi
--- vim/install_pathogen.sh
+++ vim/install_pathogen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
