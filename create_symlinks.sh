7e502416342cfef6ca621be4264adf7592943465
Christoph Gockel <github@christophgockel.de>
2016-11-17 00:03:34 +0000

    Refactors creation of symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,40 +3,24 @@ set -euo pipefail
 
 base_dir=$(dirname "$PWD/$0")
 
-if [ ! -f $HOME/.vimrc ]; then
-  echo "Linking .vimrc"
-  ln -s $base_dir/vim/vimrc $HOME/.vimrc
-else
-  echo "Skipping .vimrc"
-fi
+function create_link_if_needed {
+  local source=$1
+  local target=$2
+  local target_file=$(basename $target)
 
-if [ ! -f $HOME/.zshrc ]; then
-  echo "Linking .zshrc"
-  ln -s $base_dir/zsh/zshrc $HOME/.zshrc
-else
-  echo "Skipping .zshrc"
-fi
+  if [ ! -f $target ]; then
+    echo "Linking $target_file"
+    ln -s $source $target
+  else
+    echo "Skipping $target_file"
+  fi
+}
 
-if [ ! -f $HOME/.tmux.conf ]; then
-  echo "Linking .tmux.conf"
-  ln -s $base_dir/tmux/tmux.conf $HOME/.tmux.conf
-else
-  echo "Skipping .tmux.conf"
-fi
-
-if [ ! -f $HOME/.gitconfig ]; then
-  echo "Linking .gitconfig"
-  ln -s $base_dir/git/gitconfig $HOME/.gitconfig
-else
-  echo "Skipping .gitconfig"
-fi
-
-if [ ! -f $HOME/.gitignore_global ]; then
-  echo "Linking .gitignore_global"
-  ln -s $base_dir/git/gitignore_global $HOME/.gitignore_global
-else
-  echo "Skipping .gitignore_global"
-fi
+create_link_if_needed "$base_dir/vim/vimrc", "$HOME/.vimrc"
+create_link_if_needed "$base_dir/zsh/zshrc", "$HOME/.zshrc"
+create_link_if_needed "$base_dir/tmux/tmux.conf", "$HOME/.tmux.conf"
+create_link_if_needed "$base_dir/git/gitconfig", "$HOME/.gitconfig"
+create_link_if_needed "$base_dir/git/gitignore_global", "$HOME/.gitignore_global"
 
 if [ ! -f $HOME/.vim/colors/base16-default-dark.vim ]; then
   echo "Linking vim colors"
@@ -44,3 +28,4 @@ if [ ! -f $HOME/.vim/colors/base16-default-dark.vim ]; then
 else
   echo "Skipping vim colors"
 fi
+
