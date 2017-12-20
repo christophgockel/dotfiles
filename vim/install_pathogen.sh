1811e17b207183ffe4cae4049a10bd67f2e9b228
Christoph Gockel <github@christophgockel.de>
2017-12-20 12:14:56 +0000

    Replaces Pathogen with vim-plug

--- vim/install_pathogen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
-curl -LSso $HOME/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
