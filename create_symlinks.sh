cf4e584357e6fd26ac0b5249477dfa877d7509d2
Christoph Gockel <github@christophgockel.de>
2017-12-22 14:04:52 +0000

    Fixes symlink script for zsh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-set -euo pipefail
+set -euo
 
 base_dir=$(dirname "$PWD/$0")
 
-function create_link_if_needed {
+create_link_if_needed() {
   local source=$1
   local target=$2
   local target_file=$(basename $target)
@@ -21,11 +21,4 @@ create_link_if_needed "$base_dir/zsh/zshrc" "$HOME/.zshrc"
 create_link_if_needed "$base_dir/tmux/tmux.conf" "$HOME/.tmux.conf"
 create_link_if_needed "$base_dir/git/gitconfig" "$HOME/.gitconfig"
 create_link_if_needed "$base_dir/git/gitignore_global" "$HOME/.gitignore_global"
-
-if [ ! -f $HOME/.vim/colors/base16-default-dark.vim ]; then
-  echo "Linking vim colors"
-  ln -s $HOME/.vim/bundle/base16-vim/colors/* $HOME/.vim/colors/
-else
-  echo "Skipping vim colors"
-fi
 
