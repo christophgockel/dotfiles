fa22f4df6b550b8c724aeabc6e0e37d8adc939ba
Christoph Gockel <git@christophgockel.de>
2022-06-24 23:16:19 +0100

    Updates base16 remote URLs

--- base16/install.sh
+++ base16/install.sh
@@ -7,7 +7,7 @@ target_dir="$base_dir/base16-iterm2"
 if [ ! -d "$target_dir" ]; then
   echo "cloning base16-iterm2 into ${target_dir}"
   mkdir -p $target_dir
-  git clone https://github.com/chriskempson/base16-iterm2.git $target_dir
+  git clone https://github.com/base16-project/base16-iterm2.git $target_dir
 else
   echo "base16-iterm2 already cloned"
 fi
@@ -18,7 +18,7 @@ target_dir="$base_dir/base16-shell"
 if [ ! -d "$target_dir" ]; then
   echo "cloning base16-shell into ${target_dir}"
   mkdir -p $target_dir
-  git clone https://github.com/chriskempson/base16-shell.git $target_dir
+  git clone https://github.com/base16-project/base16-shell.git $target_dir
 else
   echo "base16-shell already cloned"
 fi
